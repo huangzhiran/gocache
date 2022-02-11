3e83bca42bd3a5bba6e24389b27ca2e3f75a535d
huangzhiran <599953360@qq.com>
2022-02-11 02:00:03 +0000

    add lru get unit test

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -1,1 +1,25 @@
-package cache
+package cache_test
+
+import (
+	"testing"
+
+	"github.com/huangzhiran/gocache/cache"
+)
+
+type String string
+
+func (s String) Len() int {
+	return len(s)
+}
+
+func TestLruGet(t *testing.T) {
+	c := cache.NewLru(10, nil)
+	c.Add("key1", String("1111"))
+	v, ok := c.Get("key1")
+	if !ok || string(v.(String)) != "1111" {
+		t.Fatalf("cache hit key1=1111 failed")
+	}
+	if _, ok := c.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
